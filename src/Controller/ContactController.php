ab03950a7d837df44dfbcfdbc415016cd5d055ba
torr <newmyclio2004>
2019-08-05 21:58:31 +0300

    contact form

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -2,20 +2,42 @@
 
 namespace App\Controller;
 
+use App\Entity\Message;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
 
 class ContactController extends AbstractController
 {
     /**
-     * @param Request $request
+     * @param Request            $request
+     * @param ValidatorInterface $validator
      * @return JsonResponse
      */
-    public function sendContactAction(Request $request): JsonResponse
+    public function sendContactAction(Request $request, ValidatorInterface $validator): JsonResponse
     {
-        $data = $request->get('data');
-        $result = [];
-        return $this->json(['data' => $result]);
+        $name = $request->get('name');
+        $email = $request->get('email');
+        $text = $request->get('message');
+
+        $message = new Message();
+        $message->setSenderName($name);
+        $message->setSenderEmail($email);
+        $message->setText($text);
+
+        $errors = $validator->validate($message);
+
+        if ($errors->count()) {
+            $errorMessage = [];
+            foreach ($errors as $error) {
+                $errorMessage[$error->getPropertyPath()] = $error->getMessage();
+            }
+
+            return $this->json(['errors' => $errorMessage], Response::HTTP_BAD_REQUEST);
+        }
+
+        return $this->json(['data' => []]);
     }
 }

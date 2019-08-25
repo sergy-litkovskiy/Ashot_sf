ff9521e9ca3f100394c1ac81f82ed9b27d183b46
torr <newmyclio2004>
2019-08-25 16:49:56 +0300

    production version

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,12 +1,13 @@
 // const $ = require('jquery/dist/jquery.min.js');
 // const jQuery = $;
 
+require('./lib/isotope/isotope.pkgd.min.js');
 require('./lib/bootstrap/js/bootstrap.min.js');
 require('./lib/bootstrap/js/bootstrap.bundle.min.js');
-require('./lib/easing/easing.min');
-require('./lib/lightbox/js/lightbox.min');
+require('./lib/easing/easing.min.js');
+require('./lib/lightbox/js/lightbox.min.js');
 require('./lib/superfish/hoverIntent.js');
-require('./lib/touchSwipe/jquery.touchSwipe.min');
-require('./lib/waypoints/waypoints.min');
+require('./lib/touchSwipe/jquery.touchSwipe.min.js');
+require('./lib/waypoints/waypoints.min.js');
 
 require('./main.js');
